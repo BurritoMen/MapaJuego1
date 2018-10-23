edc1ad309e7c8b449c1ab3e6e5b60f9a341eed95
Nicolas Carella <esep642@gmail.com>
2018-10-23 11:11:37 -0300

    Save/load controles

--- Assets/Proyecto/Script/InputController.cs
+++ Assets/Proyecto/Script/InputController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 public class InputController : MonoBehaviour {
@@ -12,9 +14,14 @@ public class InputController : MonoBehaviour {
     private string[] KeyNames;
     public Dictionary<string,KeyCode> Keys { get; set; }
     public bool Joystick { get; set; }
+    [SerializeField]
+    private string SaveName;
+
+    private KeyCode[] SavedKeyCodes;
 
     private void Awake()
     {
+        SavedKeyCodes = new KeyCode[KeyCodes.Length];
         //para testear joysticks
         Joystick = true;
 
@@ -51,6 +58,44 @@ public class InputController : MonoBehaviour {
             Keys[KeyNames[C]] = KeyCodes[C];
           
         }
+    }
+    public void RestoreToSavedDefaults()
+    {
+        for (int C = 0; C < SavedKeyCodes.Length; C++)
+        {
+            Keys[KeyNames[C]] = SavedKeyCodes[C];
+        }
+           
+    }
+    public void Save()
+    {
+        for (int C = 0; C < KeyNames.Length; C++)
+        {
+            SavedKeyCodes[C] = Keys[KeyNames[C]];
+        }
+        string Destination = Application.persistentDataPath + SaveName;
+        Debug.Log(Destination);
+        FileStream file;
+        if (File.Exists(Destination)) file = File.OpenWrite(Destination);
+        else
+            file = File.Create(Destination);
+        BinaryFormatter bf = new BinaryFormatter();
+        bf.Serialize(file, SavedKeyCodes);
+        file.Close();
+    }
+    private void Load()
+    {
+        string Destination = Application.persistentDataPath + SaveName;
+        FileStream file;
+        if (File.Exists(Destination)) file = File.OpenRead(Destination);
+        else
+        {
+            Debug.Log("no hay archivo vieja");
+            return;
+        }
+        BinaryFormatter bf = new BinaryFormatter();
+        SavedKeyCodes = (KeyCode[])bf.Deserialize(file);
+        file.Close();
     }
 
 }
